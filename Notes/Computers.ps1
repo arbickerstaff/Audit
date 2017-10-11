f8dee060e6e35ab0cc8ae703cdc15315035daad2
Ashley Bickerstaff <arbickerstaff@gmail.com>
2017-10-11 14:17:18 +0100

    Update Computers.ps1

--- Notes/Computers.ps1
+++ Notes/Computers.ps1
@@ -1,1 +1,13 @@
 # random snips
+
+# When on the network
+
+[System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+
+[System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$ForestRootDomain = (([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()).Forest).Name
+([System.DirectoryServices.ActiveDirectory.Forest]::GetForest((New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext(‘Forest’, $ForestRootDomain)))).GetAllTrustRelationships()
+
+([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()).GetAllTrustRelationships()
+
